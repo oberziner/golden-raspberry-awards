5bc6eeffa6e6f39c5e31f6f2f9337e2869336a9a
Augusto Oberziner <augustoberziner@gmail.com>
2025-03-26 00:20:06 -0300

    Integration test

---
+++ test/test.ts
@@ -0,0 +1,36 @@
+import supertest from "supertest";
+import { buildApp } from "../src/server/start";
+
+describe("GET /user", function () {
+
+  it("responds with apr", async function() {
+    const app = await buildApp();
+    await supertest(app)
+      .get("/awards")
+      .set("Accept", "application/json")
+      .send()
+      .expect("Content-Type", /json/)
+      .expect(
+        200,
+        {
+          min: [
+            {
+              producer_name: 'Joel Silver',
+              interval: 1,
+              followingWin: 1991,
+              previousWin: 1990
+            }
+          ],
+          max: [
+            {
+              producer_name: 'Matthew Vaughn',
+              interval: 13,
+              followingWin: 2015,
+              previousWin: 2002
+            }
+          ]
+        },
+      )
+  });
+});
+
